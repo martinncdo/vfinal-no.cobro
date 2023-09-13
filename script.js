c40e394377d2761340a79fe286cffa4f58d564af
Cristaldo Martín <cristaldopmartin@gmail.com>
2023-09-13 17:45:36 -0300

    Cambios en el formulario de contacto

--- script.js
+++ script.js
@@ -1,1 +1,96 @@
-let animado=document.querySelectorAll(".animado");function mostrarScroll(){let e=document.documentElement.scrollTop;for(var t=0;1<animado.length;t++)animado[t].offsetTop-510<e&&(animado[t].style.opacity=1,animado[t].classList.add("mostrarArriba"))}function hamburgerMenu(e,t,a){let s=document;s.addEventListener("click",o=>{(o.target.matches(e)||o.target.matches(`${e} *`))&&(s.querySelector(t).classList.toggle("is-active"),s.querySelector(e).classList.toggle("is-active")),o.target.matches(a)&&(s.querySelector(t).classList.remove("is-active"),s.querySelector(e).classList.remove("is-active"))})}window.addEventListener("scroll",mostrarScroll),window.addEventListener("load",function(){let e=document.querySelectorAll(".fadeInLeft"),t=document.querySelectorAll(".fadeInRight");t.forEach(function(e,t){setTimeout(function(){e.classList.add("active")},100*t)}),e.forEach(function(e,t){setTimeout(function(){e.classList.add("active")},100*t)})}),window.addEventListener("load",()=>{texto=document.getElementById("fuente"),setTimeout(()=>{texto.classList.add("titulo-efecto")},500)}),hamburgerMenu(".panel-btn",".panel",".menu a");const d=document,$form=d.querySelector(".contact-form"),$loader=d.querySelector(".contact-form-loader"),$response=d.querySelector(".contact-form-response");d.addEventListener("submit",async e=>{if(e.preventDefault(),e.target==$form)try{$loader.classList.remove("none");let t=await fetch("https://formsubmit.co/ajax/ca3d64ec31dd5a263771855b0870f3f8",await {method:"POST",body:new FormData(e.target)});if(console.log(t.json()),$loader.classList.add("none"),$response.classList.remove("none"),setTimeout(e=>{$response.classList.add("none")},3e3),$form.reset(),!t.ok)return{status:t.status,msg:t.statusText}}catch(a){$form.insertAdjacentElement("afterend",`<p style= "text-align: center;">${a.status}: ${a.msg}</p>`)}});
+let animado = document.querySelectorAll(".animado");
+
+function mostrarScroll() {
+    let scrollTop = document.documentElement.scrollTop;
+    for (var i=0; 1 < animado.length; i++) {
+        let alturaAnimado = animado[i].offsetTop;
+        if (alturaAnimado - 510 < scrollTop) {
+            animado[i].style.opacity = 1;
+            animado[i].classList.add("mostrarArriba");
+        }
+    }
+}
+
+window.addEventListener('scroll', mostrarScroll);
+
+// En tu archivo JavaScript
+window.addEventListener('load', function() {
+    // Cuando se cargue la página, obtén todos los elementos con la clase 'fadeIn'
+    const elementosLeft = document.querySelectorAll('.fadeInLeft');
+    const elementosRight = document.querySelectorAll('.fadeInRight')
+  
+    elementosRight.forEach(function(elementoRight, index) {
+        setTimeout(function() {
+          elementoRight.classList.add('active');
+        }, index * 100); // Agrega un pequeño retraso entre cada animación para que parezca más natural
+      });
+    // Agrega la clase 'active' a cada elemento después de un breve momento para iniciar la animación
+    elementosLeft.forEach(function(elementoLeft, index) {
+      setTimeout(function() {
+        elementoLeft.classList.add('active');
+      }, index * 100); // Agrega un pequeño retraso entre cada animación para que parezca más natural
+    });
+  });
+
+window.addEventListener('load', ()=> {
+  texto = document.getElementById("fuente");
+  setTimeout(()=>{
+      texto.classList.add("titulo-efecto");
+  }, 500);
+});
+
+//función navbar responsive
+
+function hamburgerMenu(panelBtn, panel, menuLink){
+  const d = document;
+
+  d.addEventListener("click", e => {
+      if(e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)){
+          d.querySelector(panel).classList.toggle("is-active");
+          d.querySelector(panelBtn).classList.toggle("is-active");    
+          }
+          
+      if (e.target.matches(menuLink)){
+          d.querySelector(panel).classList.remove("is-active");
+          d.querySelector(panelBtn).classList.remove("is-active");
+      }
+
+      });
+  }
+
+hamburgerMenu(".panel-btn",".panel",".menu a");
+
+// Envío de email
+
+const d = document,
+            $form = d.querySelector(".contact-form"),
+            $loader = d.querySelector(".contact-form-loader"),
+            $response = d.querySelector(".contact-form-response");
+
+        d.addEventListener("submit", async (e) => {
+            e.preventDefault();
+            if (e.target == $form) {
+                try {
+                    $loader.classList.remove("none");
+                    let data = await {
+                        method: "POST",
+                        body: new FormData(e.target)
+                    },
+                    res = await fetch("https://formsubmit.co/ajax/ca3d64ec31dd5a263771855b0870f3f8", data);
+                    console.log(res.json())
+                    $loader.classList.add("none");
+                    $response.classList.remove("none");
+                    setTimeout(e => {
+                        $response.classList.add("none");
+                    }, 3000);
+                    $form.reset();
+                    
+                    if(!res.ok) return {status: res.status, msg: res.statusText};
+                } catch(err) {
+                    $form.insertAdjacentElement("afterend", `<p style= "text-align: center;">${err.status}: ${err.msg}</p>`);
+                }
+            }
+        }
+        );
+
+
